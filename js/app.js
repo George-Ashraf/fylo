f27bf9b6297d89f5f0d5f1a0b7a3f97dc1198e7a
George-Ashraf <georgeid20@gmail.com>
2022-10-30 23:16:34 +0200

    first commit

---
+++ js/app.js
@@ -0,0 +1,21 @@
+wow = new WOW(
+    {
+    boxClass:'animate__animated'
+    
+  }).init();
+// wow js
+  $(document).ready(function(){
+  
+  
+    $("body").niceScroll({
+        cursorcolor: "WHITE",
+        cursorwidth:5
+    });
+    //nice scroll
+    
+  });
+  var scroll = new SmoothScroll('a[href*="#"]',{
+    speed: 1000
+  });
+ 
+  //smooth scroll
